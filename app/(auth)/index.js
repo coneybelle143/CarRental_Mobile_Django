c71ed7a3c4352c4f49afb66d874b874bea570b82
Coderist1 <Mathewpolinar5@gmail.com>
2026-05-12 08:48:05 +0800

    Remove BoardEase/boarding house files - keep only CarRentalMobile

--- app/(auth)/index.js
+++
@@ -1,6 +0,0 @@
-import { Redirect } from 'expo-router';
-
-export default function AuthIndex() {
-  // Redirect to the tenant signup by default
-  return <Redirect href="/(auth)/tenant-signup" />;
-}
